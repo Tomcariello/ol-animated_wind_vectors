1fdb1564ab0cde6fb818f3216af6ce4855dcad0d
Tom Cariello <tomcariello@gmail.com>
2022-11-30 10:29:38 -0500

    I have GLSL drawing on the screen FINALLYgit add .!

--- main.js
+++ main.js
@@ -12,28 +12,44 @@ import WebGLPointsLayerRenderer from 'ol/renderer/webgl/PointsLayer';
 class CustomWebGLPointsLayer extends WebGLPointsLayer {
   createRenderer() {
     return new WebGLPointsLayerRenderer(this, { 
+      // "attributes" are values that are passed into the shaders
       attributes: [
         {
-          name: 'coordinates',
+          name: 'a_coords',
           callback: function(feature) {
-            return Math.floor(Math.random() * 100);
+            // This callback is executed for each feature
+            
+            // Create an array of the X & Y positions for GLSL.
+            const coordinatesArr = [ feature.values_[0],feature.values_[1] ];
+
+            return coordinatesArr;
           }
         },
       ],
-      // This crashes if the vertexShader or fragmentShader fail to compile
-      // But nothing is drawn to the screen regardless of the values below.
-      vertexShader: `
-        attribute vec4 a_position;
-        
-        void main() {
-          gl_Position = a_position;
-        }`,
+      // These are the default Open Layers Shaders
+      // https://openlayers.org/en/latest/apidoc/module-ol_webgl_PostProcessingPass-WebGLPostProcessingPass.html
+      vertexShader: 
+      `
+      precision mediump float;
+
+      attribute vec2 a_coords;
+      varying vec2 v_texCoord;
+      varying vec2 v_screenCoord;
+      
+      uniform vec2 u_screenSize;
+      
+      void main() {
+        v_texCoord = a_coords * 0.5 + 0.5;
+        v_screenCoord = v_texCoord * u_screenSize;
+        gl_Position = vec4(a_coords, 0.0, 1.0);
+      }
+      `,
+      // This should paint all fragments green (values are RGB + Alpha)
       fragmentShader: `
-        precision mediump float;
-        
-        void main() {
-          gl_FragColor = vec4(1, 0.5, 0.75, 0.5);
-        }`
+      void main() {
+        gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
+      }
+      `
     })
   }
 };
@@ -80,7 +96,8 @@ function processData(data) {
     const { size, coord, wind } = weatherData[i];
 
     // Convert from lon/lat to floats using the provided OL method
-    const coordinates = fromLonLat([coord.lon, coord.lat]);
+    // const coordinates = fromLonLat([coord.lon, coord.lat]);
+    const coordinates = [coord.xPos, coord.yPos];
 
     // Create this feature & add wind properties
     const feature = new Feature(new Point(coordinates));
